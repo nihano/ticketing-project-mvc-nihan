047a46633355bd13dd80e1dd84a437699453ebff
nihano <nioztekin@gmail.com>
2022-10-10 21:12:24 +0300

    role class

---
+++ src/main/java/com/cydeo/entity/Role.java
@@ -0,0 +1,14 @@
+package com.cydeo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role {
+
+    private long id;
+    private String description;
+}

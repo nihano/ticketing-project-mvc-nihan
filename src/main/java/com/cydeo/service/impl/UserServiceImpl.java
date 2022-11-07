047bc81c9982a36eefca2844f6921a270f0b19d1
nihano <nioztekin@gmail.com>
2022-11-07 14:11:49 -0500

    adding findManagers

--- src/main/java/com/cydeo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.yaml.snakeyaml.events.Event;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl extends AbstractMapService<UserDTO, String> implements UserService {
@@ -34,5 +35,12 @@ public class UserServiceImpl extends AbstractMapService<UserDTO, String> impleme
     @Override
     public void update(UserDTO object) {
         super.update(object.getUserName(), object);
+    }
+
+    @Override
+    public List<UserDTO> findManagers() {
+        return findAll().stream().filter(user->user.getRole().getId() == 2)
+                .collect(Collectors.toList());
+
     }
 }

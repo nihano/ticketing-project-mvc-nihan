5b0fd7328b4a4201a1a2bf2f4dff6f2fd47b7786
nihano <nioztekin@gmail.com>
2022-11-07 12:53:01 -0500

    Project Create page -  saving new project

---
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -0,0 +1,55 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.enums.Status;
+import com.cydeo.service.ProjectService;
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+
+    private final UserService userService;
+    private final ProjectService projectService;
+
+    public ProjectController(UserService userService, ProjectService projectService) {
+        this.userService = userService;
+        this.projectService = projectService;
+    }
+
+    @GetMapping("/create")
+    public String createProject(Model model){
+        model.addAttribute("project", new ProjectDTO());
+        model.addAttribute("managers", userService.findAll());
+        model.addAttribute("projects", projectService.findAll());
+
+        return "/project/create";
+    }
+
+    @PostMapping("/create")
+    public String insertProject(@ModelAttribute("project") ProjectDTO project){
+
+        /*
+        //when creating new project we set the Status to OPEN,
+        otherwise we will get nullPointerException.
+        Because in the html it is looking for a value.
+
+        BUT THIS IS NOT A GOOD SOLUTION BECAUSE WE ARE ADDING
+        BUSINESS LOGIC INTO CONTROLLER. WE MOVED THIS TO
+        ProjectServiceImpl
+
+        project.setProjectStatus(Status.OPEN);
+
+         */
+
+        projectService.save(project);
+        return "redirect:/project/create";//this what we see after save we will go project create again so we can use redirect
+
+    }
+}

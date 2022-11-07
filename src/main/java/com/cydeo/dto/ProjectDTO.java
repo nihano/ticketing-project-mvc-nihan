329d87e26c617290c34c402a51a2e4debb199441
nihano <nioztekin@gmail.com>
2022-11-07 12:53:34 -0500

    adding DateTimeFormatter

--- src/main/java/com/cydeo/dto/ProjectDTO.java
+++ src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -4,6 +4,7 @@ import com.cydeo.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -15,9 +16,22 @@ public class ProjectDTO {
 
     private String projectName;
     private String projectCode;
+    /*
+     //this will be saved as String in the html,
+      we are trying to parse it to UserDTO. We will use converter.
+     */
     private UserDTO assignedManager;
+
+    /*LocalDate will be saved as String in the html,
+      we are trying to parse it to LocalDate.
+      We can use DateTimeFormatter for LocalDate.
+      Pattern needs to match exact format with html,
+      (It shows format in the error page)
+     */
+    @DateTimeFormat(pattern ="yyyy-MM-dd")
     private LocalDate startDate;
+    @DateTimeFormat(pattern ="yyyy-MM-dd")
     private LocalDate endDate;
-    private String comment;
+    private String projectDetail;
     private Status projectStatus;
 }

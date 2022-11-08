8496626c62d4cc11be9d6578fd5db0b93d3b3b24
nihano <nioztekin@gmail.com>
2022-11-07 19:16:46 -0500

    adding complete update, Project Create completed.

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
 import com.cydeo.service.UserService;
@@ -57,5 +58,34 @@ public class ProjectController {
 
         return "redirect:/project/create";
 
+    }
+
+    @GetMapping("/complete/{projectCode}")
+    public String completeProject(@PathVariable("projectCode") String projectCode){
+
+        //complete -> status to complete - > do i have service?
+        projectService.complete(projectService.findById(projectCode));
+        return "redirect:/project/create";
+    }
+
+    @GetMapping("/update/{projectCode}")
+    public String editProject(@PathVariable("projectCode") String projectCode, Model model){
+        //user obj  view is requesting ${user}
+        model.addAttribute("project", projectService.findById(projectCode));//we are sending specific proj not an empty proj
+
+        //roles ${roles}
+        model.addAttribute("managers", userService.findManagers());
+
+        //users ${users}
+        model.addAttribute("projects", projectService.findAll());
+
+        return "/project/update";
+    }
+
+    @PostMapping("/update")
+    public String updateProject(@ModelAttribute("project") ProjectDTO project){
+        //update the user
+        projectService.update(project);
+        return "redirect:/project/create";
     }
 }

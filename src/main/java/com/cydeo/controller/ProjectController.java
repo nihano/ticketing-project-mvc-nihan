821e2a978f7e893b05f3e0e43425565c7f461f1a
nihano <nioztekin@gmail.com>
2022-11-08 16:59:51 -0500

    getProjectByManager added

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/project")
 public class ProjectController {
@@ -87,5 +89,14 @@ public class ProjectController {
         //update the user
         projectService.update(project);
         return "redirect:/project/create";
+    }
+
+    @GetMapping("/manager/project-status")
+    public String getProjectByManager(Model model){
+
+        UserDTO manager=userService.findById("john@cydeo.com");
+        List<ProjectDTO> projects = projectService.getCountedListOfProjectDTO(manager);
+        model.addAttribute("projects", projects);
+        return "/manager/project-status";
     }
 }

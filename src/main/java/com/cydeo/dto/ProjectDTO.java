7e81dd521ed4b432b691eff880348a349ec296c5
nihano <nioztekin@gmail.com>
2022-11-08 16:55:24 -0500

    adding completeTaskCounts and unfinishedTaskCounts fields and constructor.

--- src/main/java/com/cydeo/dto/ProjectDTO.java
+++ src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -34,4 +34,17 @@ public class ProjectDTO {
     private LocalDate endDate;
     private String projectDetail;
     private Status projectStatus;
+    private int completeTaskCounts;//added later
+    private int unfinishedTaskCounts;//added later
+
+    //constructor without last two fields to not break the code in data generator
+    public ProjectDTO(String projectName, String projectCode, UserDTO assignedManager, LocalDate startDate, LocalDate endDate, String projectDetail, Status projectStatus) {
+        this.projectName = projectName;
+        this.projectCode = projectCode;
+        this.assignedManager = assignedManager;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.projectDetail = projectDetail;
+        this.projectStatus = projectStatus;
+    }
 }
